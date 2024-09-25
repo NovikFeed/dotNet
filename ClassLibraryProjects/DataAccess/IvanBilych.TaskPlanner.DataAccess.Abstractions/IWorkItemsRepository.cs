0a88d322804dd2fff711cb7952e3592e9005712e
NovikFeed <vania200345@gmail.com>
2024-09-25 21:31:10 +0300

    Initial commit

---
+++ ClassLibraryProjects/DataAccess/IvanBilych.TaskPlanner.DataAccess.Abstractions/IWorkItemsRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace IvanBilych.TaskPlanner.DataAccess.Abstractions;
+
+using System;
+using IvanBilych.TaskPlanner.Domain.Models;
+
+public interface IWorkItemsRepository
+{
+  Guid Add(WorkItem workItem);
+  WorkItem? Get(Guid id);
+  WorkItem[] GetAll();
+  bool Update(WorkItem workItem);
+  bool Remove(Guid id);
+  void SaveChanges();
+}
---
+++ ClassLibraryProjects/Domain/IvanBilych.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,29 @@
+﻿using IvanBilych.TaskPlanner.Domain.Models;
+using IvanBilych.TaskPlanner.DataAccess.Abstractions;
+
+namespace IvanBilych.TaskPlanner.Domain.Logic;
+
+
+public class SimpleTaskPlanner
+{
+  public WorkItem[] CreatePlan(IWorkItemsRepository fileWorkItemsRepository)
+  {
+    List<WorkItem> workItemList = fileWorkItemsRepository.GetAll().ToList();
+
+    workItemList.Sort((x, y) =>
+        {
+          int priorityComparison = y.Priority.CompareTo(x.Priority);
+          if (priorityComparison != 0)
+            return priorityComparison;
+
+          int dueDateComparison = x.DueDate.CompareTo(y.DueDate);
+          if (dueDateComparison != 0)
+            return dueDateComparison;
+
+          return string.Compare(x.Title, y.Title, StringComparison.OrdinalIgnoreCase);
+        });
+
+    return workItemList.Where(x => x.IsCompleted is false).ToArray();
+  }
+}
+
